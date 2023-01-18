932134fd7de20f716f0ab54846056214ea0d8db7
Dharmendra <dharmendra.patel264@gmail.com>
2023-01-18 18:38:00 +0530

    firstcommit

--- _helpers/db.js
+++ _helpers/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
+
 module.exports = {
     Agency: require('../agencyClient/agency.model'),
     Client: require('../agencyClient/client.model')
